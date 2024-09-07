1e6f86865e25865c7fc9da9e3312147cab66f5d0
Tristan Protzman <tlprotzman@gmail.com>
2024-09-07 14:08:31 +0200

    more plots?

--- monitoring.py
+++ monitoring.py
@@ -1,21 +1,52 @@
 import sys
-sys.argv.append('-b') # load root in batch mode
 import ROOT
+ROOT.gROOT.SetBatch(True)
 import time
+import signal
+import os
+import random
+
+running = True
+
+# Define a signal handler function
+def handle_stop_signal(signal, frame):
+    global running
+    running = False
+    print("Stop signal received. Exiting...")
 
 class hit:
     def __init__(self, board, channel, low_gain, high_gain, timestamp, trigger_id):
         self.board = int(board)
         self.channel = int(channel)
+        
+        self.gain_ratio = 9.5
         self.low_gain = int(low_gain)
         self.high_gain = int(high_gain)
+        self.comb_gain = int(high_gain)
+        if self.comb_gain >= 3800:
+            self.comb_gain = (self.low_gain + int(random.random()))* self.gain_ratio
+        
         self.timestamp = float(timestamp)
         self.trigger_id = int(trigger_id)
 
+        self.x = (self.channel % 8)
+        if self.x > 3:
+            self.x = 7 - self.x
+
+        self.y = (self.channel % 8)
+        if self.y > 3:
+            self.y = 0
+        else:
+            self.y = 1
+
+        self.z = self.board * 8 + (7 - int(self.channel / 8))
+
+    
 
 class file_parser:
     def __init__(self, file_path):
         self.file_path = file_path
+        self.line_number = 0
 
     # Define context manager methods
     def __enter__(self):
@@ -23,6 +54,7 @@ class file_parser:
         # Skip first 9 lines
         for _ in range(9):
             self.file.readline()
+            self.line_number += 1
         return self
     
     def __exit__(self, exc_type, exc_value, traceback):
@@ -34,27 +66,41 @@ class file_parser:
     
     def __next__(self):
         # First, get the summary line
-        line = self.file.readline().strip()
-        if not line:
-            return None
-            # raise StopIteration
-        board, channel, low_gain, high_gain, timestamp, trigger_id, nhits = line.split()
-        hits = [None] * int(nhits)
-        hits[int(channel)] = hit(board, channel, low_gain, high_gain, timestamp, trigger_id)
-        
-        # Then, get the rest of the hits
-        for i in range(1, int(nhits)):
+        try:
             line = self.file.readline().strip()
+            self.line_number += 1
             if not line:
                 return None
-            board, channel, low_gain, high_gain = line.split()
+                # raise StopIteration
+            board, channel, low_gain, high_gain, timestamp, trigger_id, nhits = line.split()
+            hits = [None] * int(nhits)
             hits[int(channel)] = hit(board, channel, low_gain, high_gain, timestamp, trigger_id)
-        return hits
+            
+            # Then, get the rest of the hits
+            for i in range(1, int(nhits)):
+                line = self.file.readline().strip()
+                self.line_number += 1
+                if not line:
+                    return None
+                board, channel, low_gain, high_gain = line.split()
+                hits[int(channel)] = hit(board, channel, low_gain, high_gain, timestamp, trigger_id)
+            return hits
+        except ValueError:
+            print(f"Error parsing line {self.line_number}: {line}")
+            return None
     
 
 
 class online_monitor:
-    def __init__(self, file_path, caen_units=8, channels=64):
+    def __init__(self, file_path, run_number, caen_units=8, channels=64):
+        # Set up the output file
+        output_path = f"output/run{run_number}"
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+        
+        self.output_file = ROOT.TFile(f"{output_path}/run{run_number}.root", "RECREATE")
+
+
         self.file_path = file_path
         self.caen_units = caen_units
         self.channels = channels
@@ -62,9 +108,22 @@ class online_monitor:
         
         self.low_gain_histograms = []
         self.high_gain_histograms = []
+        self.combined_gain_histograms = []
         self.gain_correlations = []
 
         self.num_events = []
+        self.missed_events = []
+        self.num_hits = [0] * caen_units
+        self.last_hits = []
+        for i in range(caen_units):
+            self.last_hits.append([hit(i, 0, 0, 0, 0, 0)] * channels)
+
+        self.overview_high_gain = None
+        self.overview_low_gain = None
+        self.overview_combined_gain = None
+
+        self.event_display = None
+        self.event_display_combined_gain = None
 
         self.canvases = []
 
@@ -72,91 +131,236 @@ class online_monitor:
         for i in range(caen_units):
             caen_lg_histograms = []
             caen_hg_histograms = []
+            caen_cg_histograms = []
             caen_correlation_histograms = []
+
+
             for j in range(channels):
                 lg_histogram = ROOT.TH1F(f'caen_{i}_ch_{j}_lg', f'CAEN {i} Channel {j} Low Gain', 4096, 0, 4096)
                 hg_histogram = ROOT.TH1F(f'caen_{i}_ch_{j}_hg', f'CAEN {i} Channel {j} High Gain', 4096, 0, 4096)
+                cb_histogram = ROOT.TH1F(f'caen_{i}_ch_{j}_cb', f'CAEN {i} Channel {j} Combined Gain', 40001, 0, 40001)
                 correlation_hist = ROOT.TH2F(f'caen_{i}_ch_{j}_correlation', f'CAEN {i} Channel {j} Correlation;High Gain;Low Gain', 512, 0, 4096, 256, 0, 512)
                 self.server.Register(f'/individual/low_gain/caen_{i}', lg_histogram)
                 self.server.Register(f'/individual/high_gain/caen_{i}', hg_histogram)
+                self.server.Register(f'/individual/combined_gain/caen_{i}', cb_histogram)
                 self.server.Register(f'/individual/correlation/caen_{i}', correlation_hist)
                 caen_lg_histograms.append(lg_histogram)
                 caen_hg_histograms.append(hg_histogram)
+                caen_cg_histograms.append(cb_histogram)
                 caen_correlation_histograms.append(correlation_hist)
                 
 
             self.low_gain_histograms.append(caen_lg_histograms)
             self.high_gain_histograms.append(caen_hg_histograms)
+            self.combined_gain_histograms.append(caen_cg_histograms)
             self.gain_correlations.append(caen_correlation_histograms)
 
         for i in range(caen_units):
             lg_canvas = ROOT.TCanvas(f'caen_{i}_lg', f'CAEN {i} Low Gain', 1200, 800)
             hg_canvas = ROOT.TCanvas(f'caen_{i}_hg', f'CAEN {i} High Gain', 1200, 800)
+            cb_canvas = ROOT.TCanvas(f'caen_{i}_cb', f'CAEN {i} Combined Gain', 1200, 800)
             corr_canvas = ROOT.TCanvas(f'caen_{i}_corr', f'CAEN {i} Correlation', 1200, 800)
             lg_canvas.Divide(8, 8, 0, 0)
             hg_canvas.Divide(8, 8, 0, 0)
+            cb_canvas.Divide(8, 8, 0, 0)
             corr_canvas.Divide(8, 8, 0, 0)
             for j in range(channels):
+                # Draw low gain
                 lg_canvas.cd(j+1)
                 self.low_gain_histograms[i][j].Draw()
                 ROOT.gPad.SetLogy()
+                # Draw high gain
                 hg_canvas.cd(j+1)
                 ROOT.gPad.SetLogy()
                 self.high_gain_histograms[i][j].Draw()
+                # Draw combined gain
+                cb_canvas.cd(j+1)
+                ROOT.gPad.SetLogy()
+                self.combined_gain_histograms[i][j].Draw()
+                # Draw correlation
                 corr_canvas.cd(j+1)
                 self.gain_correlations[i][j].Draw('col')
 
             self.server.Register(f'/overview/low_gain', lg_canvas)
             self.server.Register(f'/overview/high_gain', hg_canvas)
+            self.server.Register(f'/overview/combined_gain', cb_canvas)
             self.server.Register(f'/overview/correlation', corr_canvas)
             self.canvases.append(lg_canvas)
             self.canvases.append(hg_canvas)
+            self.canvases.append(cb_canvas)
             self.canvases.append(corr_canvas)
 
         multi_graph = ROOT.TMultiGraph()
-        for i in range(caen_units):
-            number_events = ROOT.TGraph()
-            number_events.SetTitle(f'CAEN {i} Number of Events')
-            number_events.SetName(f'caen_{i}_num_events')
-            self.num_events.append(number_events)
-            # ROOT.gROOT.Add(number_events)
-            multi_graph.Add(number_events)
-        
-        canvas = ROOT.TCanvas('num_events', 'Number of Events', 1200, 800)
-        canvas.cd()
+        # i = 0
+        canvas = ROOT.TCanvas(f'num_events_caen', f'Number of Events', 1200, 800)
+        canvas_2 = ROOT.TCanvas(f'missed_events_caen', f'Missed Events', 1200, 800)
+        line_colors = [ROOT.kRed, ROOT.kBlue, ROOT.kGreen + 2, ROOT.kMagenta]
+        line_styles = [ROOT.kSolid, ROOT.kDashed]
+        line_styles_2 = [ROOT.kDotted, ROOT.kDashDotted]
+        legend = ROOT.TLegend(0.1, 0.7, 0.48, 0.9)
+        legend.SetLineWidth(0)
+        self.canvases.append(legend)
         self.canvases.append(canvas)
-        multi_graph.Draw('AP')
-        self.num_events.append(multi_graph)
-        self.server.Register('/overview/num_events', canvas)
+        self.canvases.append(canvas_2)
+        for i in range(caen_units):
+            canvas.cd()
+            number_events = ROOT.TGraph()
+            number_events.SetTitle(f'Number of Events Run {run_number}')
+            number_events.SetName(f'caen_{i}_num_events')
+            number_events.GetXaxis().SetTimeDisplay(1)
+            number_events.GetXaxis().SetTimeFormat('%H:%M:%S')
+            number_events.SetLineColor(line_colors[i % 4])
+            number_events.SetLineStyle(line_styles[int(i / 4)])
+            number_events.SetLineWidth(2)
+            legend.AddEntry(number_events, f'CAEN {i}', 'l')
+            if i == 0:
+                number_events.Draw("AL")
+            else:
+                number_events.Draw("L")
 
+
+            canvas_2.cd()
+            ROOT.gROOT.Add(number_events)
+            self.num_events.append(number_events)
+            missed_events = ROOT.TGraph()
+            missed_events.SetTitle(f'Fraction of Events Run {run_number}')
+            missed_events.SetName(f'caen_{i}_num_events')
+            missed_events.GetXaxis().SetTimeDisplay(1)
+            missed_events.GetXaxis().SetTimeFormat('%H:%M:%S')
+            missed_events.SetLineColor(line_colors[i % 4])
+            missed_events.SetLineStyle(line_styles_2[int(i / 4)])
+            missed_events.SetLineWidth(2)
+            if i == 0:
+                missed_events.Draw("AL")
+            else:
+                missed_events.Draw("L")
+            missed_events.GetYaxis().SetRangeUser(0, 1)
+            ROOT.gROOT.Add(missed_events)
+            self.missed_events.append(missed_events)
+        canvas.cd()
+        legend.Draw()
+        canvas_2.cd()
+        legend.Draw()
+        self.server.Register('/overview/num_events', canvas)
+        self.server.Register('/overview/num_events', canvas_2)
+        # multi_graph.Add(number_events)
+
+        canvas = ROOT.TCanvas(f'Overview HG', f'Overview HG', 1200, 800)
+        canvas.SetLogz()
+        self.overview_high_gain = ROOT.TH2F('overview_high_gain', 'Overview HG', caen_units*100, 0, caen_units*100, 4096, 0, 4096)
+        self.overview_high_gain.Draw("COLZ")
+        self.server.Register('/overview/AllAtOnce', canvas)
+        self.canvases.append(canvas)
+
+        canvas2 = ROOT.TCanvas(f'Overview LG', f'Overview LG', 1200, 800)
+        canvas2.SetLogz()
+        self.overview_low_gain = ROOT.TH2F('overview_low_gain', 'Overview LG', caen_units*100, 0, caen_units*100, 4096, 0, 4096)
+        self.overview_low_gain.Draw("COLZ")
+        self.server.Register('/overview/AllAtOnce', canvas2)
+        self.canvases.append(canvas2)
+
+        canvas3 = ROOT.TCanvas(f'Overview CombG', f'Overview CombG', 1200, 800)
+        canvas3.SetLogz()
+        self.overview_combined_gain = ROOT.TH2F('overview_comb_gain', 'Overview CombG', caen_units*100, 0, caen_units*100, 40000, 0, 40000)
+        self.overview_combined_gain.Draw("COLZ")
+        self.server.Register('/overview/AllAtOnce', canvas3)
+        self.canvases.append(canvas3)
         
+        # canvas = ROOT.TCanvas('num_events', 'Number of Events', 1200, 800)
+        # canvas.cd()
+        # multi_graph.GetXaxis().SetTimeDisplay(1)
+        # multi_graph.GetXaxis().SetTimeFormat('%H:%M:%S')
+        # multi_graph.Draw('AL PLC')
+        # self.server.Register('/overview/num_events', canvas)
+        # self.num_events.append(multi_graph)
+
+        canvas = ROOT.TCanvas('event_display', 'Event Display', 1200, 800)
+        self.event_display = ROOT.TH3F('event_display', 'Event Display', 64, 0, 64, 4, 0, 4, 2, 0, 2)
+        # self.event_display.SetMinimum(0)
+        # self.event_display.SetMaximum(4096)
+        self.event_display.Draw("BOX2Z")
+        self.server.Register('/overview/event_display', canvas)
+        self.canvases.append(canvas)
+
+        canvas2 = ROOT.TCanvas('event_display_comb', 'Event Display Comb', 1200, 800)
+        self.event_display_comb = ROOT.TH3F('event_display_comb', 'Event Display Comb gain', 66, 0, 66, 4, 0, 4, 2, 0, 2)
+        # self.event_display_comb.SetMinimum(0)
+        # self.event_display_comb.SetMaximum(40000)
+        self.event_display_comb.SetStats(0);
+        self.event_display_comb.Draw("BOX2Z")
+        self.server.Register('/overview/event_display', canvas2)
+        self.canvases.append(canvas2)
+
+    def close(self):
+        self.output_file.Write()
+        self.output_file.Close()
+
+    def update(self):
+        max = 0
+        for i in range(self.caen_units):
+            self.num_events[i].SetPoint(self.num_events[i].GetN(), ROOT.TDatime().Convert(), self.last_hits[i][0].trigger_id)
+            if self.last_hits[i][0].trigger_id > max:
+                max = self.last_hits[i][0].trigger_id
+        self.num_events[i].GetYaxis().SetRangeUser(0, max * 1.2)
+        for i in range(self.caen_units):
+            if (max == 0):
+                continue
+            self.missed_events[i].SetPoint(self.missed_events[i].GetN(), ROOT.TDatime().Convert(), 1 - (self.num_hits[i] / max))
 
     def event_loop(self):
-        # events = []
         with file_parser(self.file_path) as parser:
             for hits in parser:
+                global running
+                if (running == False):
+                    print(f"Exiting is {running}")
+                    break
                 self.server.ProcessRequests()
                 if hits is None:
                     print('sleeping')
-                    time.sleep(0.2)
+                    self.update()
+                    self.make_event_display()
+                    time.sleep(2)
                     continue
-                print('reading')
-                t = ROOT.TDatime().Convert()
-                self.num_events[hits[0].board].SetPoint(self.num_events[hits[0].board].GetN(), t, hits[0].trigger_id)
+                # print('reading')
+                self.num_hits[hits[0].board] += 1
                 for hit in hits:
+                    self.last_hits[hit.board][hit.channel] = hit
                     self.low_gain_histograms[hit.board][hit.channel].Fill(hit.low_gain)
                     self.high_gain_histograms[hit.board][hit.channel].Fill(hit.high_gain)
+                    self.combined_gain_histograms[hit.board][hit.channel].Fill(hit.comb_gain)
                     self.gain_correlations[hit.board][hit.channel].Fill(hit.high_gain, hit.low_gain)
+                    self.overview_high_gain.Fill(hit.board * 100 + hit.channel, hit.high_gain)
+                    self.overview_low_gain.Fill(hit.board * 100 + hit.channel, hit.low_gain)
+                    self.overview_combined_gain.Fill(hit.board * 100 + hit.channel, hit.comb_gain)
+
+                t = ROOT.TDatime().Convert()
+                # print(f'Filling board {hits[0].board} at time {t} with {hits[0].trigger_id}')
                 # events.append(hits)
+
+
+    def make_event_display(self):
+        self.event_display.Reset()
+        for i in range(2):
+            for j in range(4):
+                self.event_display.Fill(0, i, j, 0)
+                self.event_display.Fill(66, i, j, 4096)
+        for i in range(self.caen_units):
+            for j in range(self.channels):
+                hit = self.last_hits[i][j]
+                # print(f'Filling ({x}, {y}, {z}) with {self.last_hits[i][j].high_gain}')
+                self.event_display.Fill(hit.z + 1, hit.x, hit.y, hit.comb_gain)
         
 
     
 def main(argv):
+    # Register the signal handler
+    signal.signal(signal.SIGINT, handle_stop_signal)
     # monitor = online_monitor('data/Run68_list.txt')
     run_number = argv[1]
-    # monitor = online_monitor(f'/home/lfhcal/Downloads/Janus_5202_3.6.0_20240514_linux/bin/DataFiles/Run{run_number}_list.txt')
-    monitor = online_monitor(f'data/Run{run_number}_list.txt')
+    monitor = online_monitor(f'/home/lfhcal/Downloads/Janus_5202_3.6.0_20240514_linux/bin/DataFiles/Run{run_number}_list.txt', run_number)
+    # monitor = online_monitor(f'data/Run{run_number}_list.txt')
     monitor.event_loop()
+    monitor.close()
     pass
 
 if __name__ == '__main__':
